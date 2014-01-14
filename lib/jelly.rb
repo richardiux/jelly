61774e6df479fc97c8254952b48cd26759194ce9
Richard Millan <richardiux@gmail.com>
2014-01-13 23:18:55 -0800

    Compatibility with rails 4.0

--- lib/jelly.rb
+++ lib/jelly.rb
@@ -12,5 +12,7 @@ ActionView::Base.class_eval do
   include JellyHelper
 end
 
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jelly => ["jquery/jquery-1.3.2", "ajax_with_jelly", "jelly"]
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :only_jelly => ["jelly", "ajax_with_jelly"]
+if defined? ActionView::Helpers::AssetTagHelper.register_javascript_expansion
+  ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jelly => ["jquery/jquery-1.3.2", "ajax_with_jelly", "jelly"]
+  ActionView::Helpers::AssetTagHelper.register_javascript_expansion :only_jelly => ["jelly", "ajax_with_jelly"]
+end
